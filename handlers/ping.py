b40763aa5abd78f6be44d2eb802961a9e36e9bbe
Evgen Bratenkov <giertrut@mail.ru>
2024-07-25 20:04:52 +0300

    changed the ping file and added settings

--- handlers/ping.py
+++ handlers/ping.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter
 
+from settings import Settings
+
 
 router = APIRouter(prefix="/ping", tags=["ping"])
 
 
 @router.get("/db")
-async def ping_db():
-    return {"message": "OK"}
+async def get_db():
+    settings = Settings()
+
+    return {"db": settings.GOOGLE_TOKEN_ID}
 
 
-
-@router.get("/app")
-async def ping_app():
-    return {"text": "app is working"}
+@router.post("/app")
+async def get_app():
+    return {"app": "ok"}
 
 
---
+++ settings.py
@@ -0,0 +1,5 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    GOOGLE_TOKEN_ID: str = "qweqr12ewrwe44bffssq3"
