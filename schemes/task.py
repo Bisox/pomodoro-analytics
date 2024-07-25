786fd0f98bbd15f7bf85239ac4757b25c3aad4c4
Evgen Bratenkov <giertrut@mail.ru>
2024-07-25 20:10:53 +0300

    changed the schemes/task file

--- schemes/task.py
+++ schemes/task.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator, ValidationError, Field
 
 
 class Stask(BaseModel):
     id: int
-    name: str
-    pomodoro_count: int
-    category_id: int
+    name: str | None = None
+    pomodoro_count: int | None = None
+    category_id: int  # = Field(exclude=True)
+
+    @model_validator(mode='after')  # для выполнения пользовательских проверок на уровне модели.
+    def check_name_and_pomodoro_count(self):
+        if self.name is None and self.pomodoro_count is None:
+            raise ValidationError('Name and pomodoro count cannot be None')
+        return self
