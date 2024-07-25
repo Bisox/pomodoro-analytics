59aaac91ad87dad4edd66d35b0eec94ae9d1cdca
Evgen Bratenkov <giertrut@mail.ru>
2024-07-25 14:05:22 +0300

    added TasksCRUD

--- handlers/tasks.py
+++ handlers/tasks.py
@@ -1,13 +1,39 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
-router = APIRouter(prefix="/task", tags=["task"])
-
-
-@router.post('/{task_id}')
-async def add_task(task_id):
-    return {'added': task_id}
+from fixtures import tasks
+from schemes.task import Stask
 
 
-@router.get('/')
-async def get_task():
-    return []
+router = APIRouter(prefix="/tasks", tags=["Задачи"])
+
+
+@router.get("/all",
+            response_model=list[Stask])
+async def get_all_tasks():
+    return tasks
+
+
+@router.post("/add",
+             response_model=Stask)
+async def create_task(task: Stask):
+    tasks.append(task)
+    return task
+
+
+@router.patch("/{task_id}",
+              response_model=Stask)
+async def update_task(task_id: int, name: str):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["name"] = name
+            return task
+
+
+@router.delete("/{task_id}",
+               status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.pop(tasks.index(task))
+            return task
+    return {"message": "Task not found"}
